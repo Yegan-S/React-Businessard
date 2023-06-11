ae2fa9ade021862400eae52822911f2eb552e311
sedi <salehisediqe86@gmail.com>
2023-06-11 09:27:21 -0700

    Added two buttons and styled Info  component

--- src/Info.js
+++ src/Info.js
@@ -1,17 +1,23 @@
 import React from "react";
+import myphoto from "./image/me.jpg"
 import './App.css';
+
 
 export default function Info(){
     return (
         <div className="Info">
-        <img src="image/me.jpg" alt="myphoto"/>
+        <div className="InfoImage">
+        <img src={myphoto} alt="myphoto"
+      
+         />
+         </div>
         <div className="Name">
-        <h1>Yegan Salehi </h1>
+        <h1 >Yegan Salehi </h1>
         <h4> Frontend Developer</h4>
         </div>
         <div className="buttons">
-            <a class="btn btn-secondary" href="#" role="button">My Portfolio</a>
-            <a class="btn btn-secondary" href="#" role="button">Email</a>
+             <a class="btn btn-secondary portfolio-address" href="https://benevolent-druid-a3543f.netlify.app/" role="button">My Portfolio</a>
+            <a href="mailto:salehisediqe86@gmail.com"  class="btn btn-secondary email-address">Email </a>
 
         </div>
         </div>
