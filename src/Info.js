5f63a3168898cac343c78e613b368b4609c46fc5
sedi <salehisediqe86@gmail.com>
2023-06-12 09:17:33 -0700

    The Info component done& Created About component

--- src/Info.js
+++ src/Info.js
@@ -6,20 +6,29 @@ import './App.css';
 export default function Info(){
     return (
         <div className="Info">
+
         <div className="InfoImage">
         <img src={myphoto} alt="myphoto"
       
          />
          </div>
+
         <div className="Name">
         <h1 >Yegan Salehi </h1>
         <h4> Frontend Developer</h4>
         </div>
-        <div className="buttons">
-             <a class="btn btn-secondary portfolio-address" href="https://benevolent-druid-a3543f.netlify.app/" role="button">My Portfolio</a>
-            <a href="mailto:salehisediqe86@gmail.com"  class="btn btn-secondary email-address">Email </a>
 
+           <div className="buttons">
+            
+                  
+             <a class="btn btn-secondary portfolio-address" href="https://benevolent-druid-a3543f.netlify.app/" role="button">My Portfolio</a>
+             
+          
+            <a href="mailto:salehisediqe86@gmail.com"  class="btn btn-secondary email-address">Email </a>
+            
+     
         </div>
+
         </div>
     );
 }
